73d5b6c036233e73e8bcaff50054c2729673a528
KC <KYLE.CUSH23@bcmail.cuny.edu>
2024-11-09 10:16:30 -0500

    Added home hub for games

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # IdleStudent
 Project to create a hub of games for students to play while bored in class
+
+/IdleStudent
+	/homepage
+		/index.html
+		/index.css
+		/index.js
+	/2048
+		/2048.html
+		/2048.css
+		/2048.js
+	/pong
+		/pong.html
+		/pong.css
+		/pong.js
+	/tictactoe
+		/tictactoe.html
+		/tictactoe.css
+		/tictactoe.js
+		
---
+++ homepage/index.js
@@ -0,0 +1,13 @@
+//click event listeners for each grid item
+document.getElementById("game1").addEventListener("click", e => {
+     window.location.href = '../2048/2048.html';  
+  });
+  document.getElementById("game2").addEventListener("click", e => {
+    window.location.href = '';  
+  });
+  document.getElementById("game3").addEventListener("click", e => {
+    window.location.href = '';  
+  });
+  document.getElementById("game4").addEventListener("click", e => {
+    window.location.href = '';  
+  });
